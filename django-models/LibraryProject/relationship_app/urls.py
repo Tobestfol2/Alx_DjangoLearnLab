64efb1d626bfb8f5857130149a756032c387c4d6
Tobestfol2 <tobestfol2@gmail.com>
2025-11-10 22:52:49 +0100

    django-models

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,26 +1,10 @@
 # relationship_app/urls.py
 from django.urls import path
 from . import views
-from .admin_view import admin_view
-from .librarian_view import librarian_view
-from .member_view import member_view
 
 app_name = 'relationship_app'
 
 urlpatterns = [
-    # Existing URLs
     path('books/', views.list_books, name='list_books'),
     path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
-    path('register/', views.register_view, name='register'),
-    path('login/', views.login_view, name='login'),
-    path('logout/', views.logout_view, name='logout'),
-    path('admin/', admin_view, name='admin_view'),
-    path('librarian/', librarian_view, name='librarian_view'),
-    path('member/', member_view, name='member_view'),
-
-    # CRUD Book URLs
-    path('book/list/', views.book_list, name='book_list'),
-    path('book/add/', views.add_book, name='add_book'),
-    path('book/edit/<int:pk>/', views.edit_book, name='edit_book'),
-    path('book/delete/<int:pk>/', views.delete_book, name='delete_book'),
 ]
