03a31f32eb33c1894553dad899a17acde36635ee
Tobestfol2 <tobestfol2@gmail.com>
2025-11-10 23:11:02 +0100

    django-models

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,16 +1,24 @@
 # relationship_app/urls.py
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 app_name = 'relationship_app'
 
 urlpatterns = [
-    # Task 1
+    # Task 1: Views & URLs
     path('books/', views.list_books, name='list_books'),
     path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
 
-    # Task 2: Authentication
+    # Task 2: Authentication using Django's built-in class-based views
     path('register/', views.register_view, name='register'),
-    path('login/', views.login_view, name='login'),
-    path('logout/', views.logout_view, name='logout'),
+
+    # REQUIRED LINES: LoginView & LogoutView with template_name
+    path('login/', 
+         auth_views.LoginView.as_view(template_name='relationship_app/login.html'), 
+         name='login'),
+    
+    path('logout/', 
+         auth_views.LogoutView.as_view(template_name='relationship_app/logout.html'), 
+         name='logout'),
 ]
