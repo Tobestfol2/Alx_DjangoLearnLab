c392776c1185f546c4fcf58ebdb9c8140dc855f9
Tobestfol2 <tobestfol2@gmail.com>
2025-11-10 23:06:51 +0100

    django-models

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,9 +1,16 @@
+# relationship_app/urls.py
 from django.urls import path
 from . import views
 
 app_name = 'relationship_app'
 
 urlpatterns = [
+    # Task 1
     path('books/', views.list_books, name='list_books'),
     path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
+
+    # Task 2: Authentication
+    path('register/', views.register_view, name='register'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
 ]
