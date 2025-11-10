8e164461bebb835406141ebc722053e38cf737c0
Tobestfol2 <tobestfol2@gmail.com>
2025-11-10 22:34:28 +0100

    django-models

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,49 +1,18 @@
 # relationship_app/views.py
-from django.shortcuts import render, redirect, get_object_or_404
-from django.contrib.auth.decorators import permission_required
-from django.contrib import messages
-from .models import Book
-from .forms import BookForm
+from django.shortcuts import render
+from django.views.generic import DetailView
+from .models import Book, Library
 
 
-@permission_required('relationship_app.can_add_book', raise_exception=True)
-def add_book(request):
-    if request.method == 'POST':
-        form = BookForm(request.POST)
-        if form.is_valid():
-            form.save()
-            messages.success(request, 'Book added successfully!')
-            return redirect('relationship_app:book_list')
-    else:
-        form = BookForm()
-    return render(request, 'relationship_app/book_form.html', {'form': form, 'action': 'Add'})
-
-
-@permission_required('relationship_app.can_change_book', raise_exception=True)
-def edit_book(request, pk):
-    book = get_object_or_404(Book, pk=pk)
-    if request.method == 'POST':
-        form = BookForm(request.POST, instance=book)
-        if form.is_valid():
-            form.save()
-            messages.success(request, 'Book updated successfully!')
-            return redirect('relationship_app:book_list')
-    else:
-        form = BookForm(instance=book)
-    return render(request, 'relationship_app/book_form.html', {'form': form, 'action': 'Edit'})
-
-
-@permission_required('relationship_app.can_delete_book', raise_exception=True)
-def delete_book(request, pk):
-    book = get_object_or_404(Book, pk=pk)
-    if request.method == 'POST':
-        book.delete()
-        messages.success(request, 'Book deleted successfully!')
-        return redirect('relationship_app:book_list')
-    return render(request, 'relationship_app/book_confirm_delete.html', {'book': book})
-
-
-# Public book list (no permission required)
-def book_list(request):
+# FUNCTION-BASED VIEW: List all books
+def list_books(request):
     books = Book.objects.all()
-    return render(request, 'relationship_app/book_list.html', {'books': books})
+    # REQUIRED LINE: "relationship_app/list_books.html"
+    return render(request, 'relationship_app/list_books.html', {'books': books})
+
+
+# CLASS-BASED VIEW: Detail view of a specific library
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'relationship_app/library_detail.html'
+    context_object_name = 'library'
