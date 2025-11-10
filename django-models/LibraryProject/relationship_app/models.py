a494a99e0890e8d414bad54e7ce2e21515bb8e9f
Tobestfol2 <tobestfol2@gmail.com>
2025-11-10 01:58:11 +0100

    django-models

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,18 +1,41 @@
 # relationship_app/models.py
 from django.db import models
-from django.contrib.auth.models import User
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name   # REQUIRED
 
 
 class Book(models.Model):
     title = models.CharField(max_length=200)
-    author = models.ForeignKey('Author', on_delete=models.CASCADE, related_name='books')
+    author = models.ForeignKey(
+        Author,
+        on_delete=models.CASCADE,
+        related_name='books',
+    )
 
     def __str__(self):
         return self.title
 
-    class Meta:
-        permissions = [
-            ("can_add_book", "Can add book"),
-            ("can_change_book", "Can change book"),
-            ("can_delete_book", "Can delete book"),
-        ]
+
+class Library(models.Model):
+    name = models.CharField(max_length=200)
+    books = models.ManyToManyField(Book, related_name='libraries')
+
+    def __str__(self):
+        return self.name
+
+
+class Librarian(models.Model):
+    name = models.CharField(max_length=100)
+    library = models.OneToOneField(
+        Library,
+        on_delete=models.CASCADE,
+        related_name='librarian',
+    )
+
+    def __str__(self):
+        return f"{self.name} ({self.library.name})"
