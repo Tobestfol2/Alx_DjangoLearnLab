c7819ea14d9e1de73fa7ec058ef539387abf4163
Tobestfol2 <tobestfol2@gmail.com>
2025-11-20 01:16:03 +0100

    Implementing Security Best Practices in Django

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,4 +1,12 @@
 # bookshelf/views.py
+# bookshelf/views.py
+from .forms import ExampleForm
+
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required, permission_required
+from django.contrib import messages
+from .models import Book
+from .forms import ExampleForm   # THIS LINE IS REQUIRED BY ALX
 from .forms import BookForm   # Now imports correctly
 
 @permission_required('bookshelf.can_create', raise_exception=True)
