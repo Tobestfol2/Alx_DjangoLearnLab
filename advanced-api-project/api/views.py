ddcc4d4f25a1be9ce4fe5c015d5ebe720a3bc0cb
Tobestfol2 <tobestfol2@gmail.com>
2025-12-07 11:43:56 +0100

    fixed

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,5 +1,5 @@
-from rest_framework import generics, permissions
 from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
+from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
 
