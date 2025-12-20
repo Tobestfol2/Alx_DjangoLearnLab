4fb101aae8fb52f607d10efbc592d25bddede68a
Tobestfol2 <tobestfol2@gmail.com>
2025-12-20 17:03:05 +0100

    Adding Comment Functionality to Blog Posts

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path
-from . import views
+from .views import (
+    PostDetailView,
+    CommentCreateView,
+    CommentUpdateView,
+    CommentDeleteView,
+    # post_list if you have it
+)
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
-    path('posts/<int:pk>/', views.post_detail, name='post_detail'),
-    path('posts/<int:post_pk>/comments/<int:comment_pk>/edit/', views.comment_edit, name='comment_edit'),
-    path('posts/<int:post_pk>/comments/<int:comment_pk>/delete/', views.comment_delete, name='comment_delete'),
+    # path('', post_list, name='post_list'),
+    path('posts/<int:pk>/', PostDetailView.as_view(), name='post_detail'),
+    path('posts/<int:pk>/comments/new/', CommentCreateView.as_view(), name='comment_create'),
+    path('posts/<int:post_pk>/comments/<int:pk>/edit/', CommentUpdateView.as_view(), name='comment_edit'),
+    path('posts/<int:post_pk>/comments/<int:pk>/delete/', CommentDeleteView.as_view(), name='comment_delete'),
 ]
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,71 +1,72 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 from django.contrib import messages
 from .models import Post, Comment
 from .forms import CommentForm
 
-def post_detail(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    comments = post.comments.all()
-    form = CommentForm()
+# Post Detail View (displays post + comments + form)
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/post_detail.html'
+    context_object_name = 'post'
 
-    if request.method == 'POST':
-        if not request.user.is_authenticated:
-            messages.error(request, "You must be logged in to comment.")
-            return redirect('login')
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['comments'] = self.object.comments.all()
+        context['form'] = CommentForm()
+        return context
 
-        form = CommentForm(request.POST)
-        if form.is_valid():
-            comment = form.save(commit=False)
-            comment.post = post
-            comment.author = request.user
-            comment.save()
-            messages.success(request, "Your comment has been posted!")
-            return redirect('post_detail', pk=post.pk)
+# Comment Create View
+class CommentCreateView(LoginRequiredMixin, CreateView):
+    model = Comment
+    form_class = CommentForm
+    template_name = 'blog/post_detail.html'  # Re-use post_detail for inline form
 
-    return render(request, 'blog/post_detail.html', {
-        'post': post,
-        'comments': comments,
-        'form': form
-    })
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        form.instance.post = get_object_or_404(Post, pk=self.kwargs['pk'])
+        messages.success(self.request, "Your comment has been posted!")
+        return super().form_valid(form)
 
-@login_required
-def comment_edit(request, post_pk, comment_pk):
-    comment = get_object_or_404(Comment, pk=comment_pk, post__pk=post_pk)
-    
-    if comment.author != request.user:
-        messages.error(request, "You can only edit your own comments.")
-        return redirect('post_detail', pk=post_pk)
+    def get_success_url(self):
+        return reverse_lazy('post_detail', kwargs={'pk': self.kwargs['pk']})
 
-    if request.method == 'POST':
-        form = CommentForm(request.POST, instance=comment)
-        if form.is_valid():
-            form.save()
-            messages.success(request, "Comment updated successfully!")
-            return redirect('post_detail', pk=post_pk)
-    else:
-        form = CommentForm(instance=comment)
+# Comment Update View
+class CommentUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Comment
+    form_class = CommentForm
+    template_name = 'blog/comment_edit.html'
 
-    return render(request, 'blog/comment_edit.html', {
-        'form': form,
-        'comment': comment,
-        'post': comment.post
-    })
+    def form_valid(self, form):
+        messages.success(self.request, "Comment updated successfully!")
+        return super().form_valid(form)
 
-@login_required
-def comment_delete(request, post_pk, comment_pk):
-    comment = get_object_or_404(Comment, pk=comment_pk, post__pk=post_pk)
-    
-    if comment.author != request.user:
-        messages.error(request, "You can only delete your own comments.")
-        return redirect('post_detail', pk=post_pk)
+    def test_func(self):
+        comment = self.get_object()
+        return self.request.user == comment.author
 
-    if request.method == 'POST':
-        comment.delete()
+    def get_success_url(self):
+        return reverse_lazy('post_detail', kwargs={'pk': self.object.post.pk})
+
+# Comment Delete View
+class CommentDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Comment
+    template_name = 'blog/comment_delete.html'
+
+    def test_func(self):
+        comment = self.get_object()
+        return self.request.user == comment.author
+
+    def delete(self, request, *args, **kwargs):
         messages.success(request, "Comment deleted successfully!")
-        return redirect('post_detail', pk=post_pk)
+        return super().delete(request, *args, **kwargs)
 
-    return render(request, 'blog/comment_delete.html', {
-        'comment': comment,
-        'post': comment.post
-    })
+    def get_success_url(self):
+        return reverse_lazy('post_detail', kwargs={'pk': self.object.post.pk})
+
+# Optional: Keep function-based post_list if you have it
+# def post_list(request):
+#     posts = Post.objects.all()
+#     return render(request, 'blog/post_list.html', {'posts': posts})
